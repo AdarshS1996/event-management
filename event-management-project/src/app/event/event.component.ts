3ecd36cae26c0a35c7df6a9a87a50c4ab51b300e
adarshs-optimus <adarsh.srivastava@optimusinfo.com>
2020-10-27 15:07:49 +0530

    new design added

--- event-management-project/src/app/event/event.component.ts
+++ event-management-project/src/app/event/event.component.ts
@@ -1,7 +1,9 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, ElementRef, HostListener } from '@angular/core';
 
-declare var $: any;		
+declare var $: any;
+
+
 
 @Component({
   selector: 'app-events',
@@ -9,27 +11,9 @@ declare var $: any;
   styleUrls: ['./event.component.css']
 })
 export class EventsComponent {
-  isScrollToTopVisible: boolean = false;
 
-  constructor(private viewPortScroller: ViewportScroller) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  @HostListener('window:scroll', ['$event']) 
-    doSomething(event) {
-      if (window.pageYOffset > 8) {
-        this.isScrollToTopVisible = true;
-      } else {
-        this.isScrollToTopVisible = false;
-      }
-    }
-
-  scrollToTop() {
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-  }
-
-  onClickScroll(elementId: string) {
-    this.viewPortScroller.scrollToAnchor(elementId);
   }
 }
